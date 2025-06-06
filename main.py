42ba5c0f5d24d51cfacf83aec54caf9dc6c2e3c2
Sosa300h <209523108+Sosa300h@users.noreply.github.com>
2025-06-06 23:56:24 +0200

    collisions

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 from circleshape import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
+
+updateble = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+
+Player.containers = (updateble, drawable)
+Asteroid.containers = (asteroids, updateble, drawable)
+AsteroidField.containers = (updateble)
 
 def main():
     pygame.init
@@ -11,14 +22,20 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+    field = AsteroidField()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        updateble.update(dt)
+        for i in asteroids:
+            if player.collision(i):
+                print("Game over!")
+                sys.exit()
+        for i in drawable:
+            i.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = (clock.tick(60)) / 1000
